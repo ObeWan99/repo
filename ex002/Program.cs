f8673e42ffc0c61ac5cae43f9ee89b04273a4591
Alexander Nazimov <s-nazim@list.ru>
2023-02-11 11:17:21 +0300

    add

--- ex002/Program.cs
+++ ex002/Program.cs
@@ -1,1 +1,11 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите имя пользователя");
+string name = Console.ReadLine();
+if(name.ToLower() == "маша")
+{
+    Console.Write("УРАААА это ");
+    Console.WriteLine(name);
+}
+else{
+    Console.Write("Я не Маша, я ");
+    Console.WriteLine(name);
+}
