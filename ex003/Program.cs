1c9fa42ab50c7b4d0b06ccd773478aafd561a29a
Alexander Nazimov <s-nazim@list.ru>
2023-02-11 11:24:44 +0300

    add3

--- ex003/Program.cs
+++ ex003/Program.cs
@@ -1,6 +1,14 @@
-﻿int numberA = new Random().Next(1,10);
-Console.WriteLine(numberA);
-int numberB =new Random().Next(1,10);
-Console.WriteLine(numberB);
-int result = numberA + numberB;
-Console.WriteLine(result);
+﻿int a = 1;
+int b = 2;
+int c = 6;
+int d = 4;
+int e = 4;
+int max = a;
+if(a > max){max = a;}
+if(b > max){max = b;}
+if(c > max){max = c;}
+if(d > max){max = d;}
+if(e > max){max = e;}
+
+Console.Write("max=");
+Console.WriteLine(max);
