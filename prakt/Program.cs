ef42e2ba44a81da24293a48350dc16210887c3a3
Alexander Nazimov <s-nazim@list.ru>
2023-02-23 09:12:07 +0300

    prakt

---
+++ prakt/Program.cs
@@ -0,0 +1,31 @@
+﻿////////////////////////////////////////////////////// Задача 21
+
+/*
+Console.Clear();
+Console.WriteLine("Введите 2 координаты");
+double a = double.Parse(Console.ReadLine());
+double b = double.Parse(Console.ReadLine());
+double c = double.Parse(Console.ReadLine());
+double d = double.Parse(Console.ReadLine());
+double e = double.Parse(Console.ReadLine());
+double f = double.Parse(Console.ReadLine());
+
+double s = Math.Sqrt(Math.Pow(a-b, 2) + Math.Pow(c - d, 2)+Math.Pow(e-f, 2));
+Console.WriteLine($"{s}");
+*/
+  
+///////////////////////////////////////////////////// Задача 23
+
+/*
+Console.Clear();
+Console.WriteLine("Введите число");
+int n = int.Parse(Console.ReadLine());
+int[] arr = new int[n];
+Console.WriteLine("Получи");
+for(int i = 1; i <= n; i++)
+{
+arr[i-1]=i*i*i;
+}
+for(int i =0; i<n; i++)
+Console.WriteLine($"{arr[i]}");
+*/
