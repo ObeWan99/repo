181b863aa0473824f5fd19c93a0790d03381dc72
Alexander Nazimov <s-nazim@list.ru>
2023-02-23 09:39:16 +0300

    prakt1

--- prakt/Program.cs
+++ prakt/Program.cs
@@ -29,3 +29,29 @@ arr[i-1]=i*i*i;
 for(int i =0; i<n; i++)
 Console.WriteLine($"{arr[i]}");
 */
+
+//////////////////////////////////////// Палиндром
+
+/*
+int num, rem, sum = 0, temp;
+  
+        Console.Write("Enter a number:");
+        num = Convert.ToInt32(Console.ReadLine());
+        temp = num;
+        while (Convert.ToBoolean(num))
+        {
+            rem = num % 10;  
+            num = num / 10; 
+            sum = sum * 10 + rem; 
+                       
+        }
+        if (temp == sum) //checking whether the reversed number is equal to entered number
+        {
+            Console.WriteLine("Number is Palindrome");
+        }
+        else
+        {
+            Console.WriteLine("Number is not a palindrome");
+        }
+        Console.ReadLine();
+        */
